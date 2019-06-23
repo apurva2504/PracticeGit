12a37293685d50c2790e58cf097d09dfb3caae76
apurva <apag1996jpr@gmail,com>
2019-06-23 10:48:59 -0700

    first

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+import org.junit.Test;
+
+public class SearchPage {
+
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Hello2");
+	}
+}
---
+++ src/test/java/com/qa/testcases/LoginTest.java
@@ -0,0 +1,18 @@
+package com.qa.testcases;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void test3()
+	{
+		System.out.println("Hello3");
+	}
+}
